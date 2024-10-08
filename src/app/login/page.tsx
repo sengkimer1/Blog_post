8aca5180584380267da0545cc7428882994fb9f5
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2024-10-08 13:37:59 +0700

    login feature

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,98 @@
-import React from 'react'
+'use client'
 
-function Login() {
-  return (
-    <div>Login</div>
-  )
+import React, { useState } from "react";
+import { useRouter } from 'next/navigation';
+import { FaUser, FaLock } from 'react-icons/fa';
+import axios from "axios";
+import Button from "@/components/Button";
+import img from '@/images/background.png';
+
+interface UserFormInput {
+  email: string;
+  password: string;
 }
 
-export default Login
+function Login() {
+  const [formData, setFormData] = useState<UserFormInput>({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleLogin = async () => {
+    setLoading(true);
+    setError("");
+    try {
+      const response = await axios.post("https://students-hackaton.vercel.app/user/sign-in", {
+        email: formData.email,
+        password: formData.password,
+      });
+      const { token, user } = response.data;
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("time", new Date().getTime().toString());
+
+      router.push("/");
+    } catch (error) {
+      console.error("Login failed", error);
+      setError("Login failed. Please check your credentials and try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div
+      className="flex items-center justify-center min-h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `URL(${img.src})`,
+      }}
+    >
+      <div className="flex items-center justify-center min-h-screen bg-cover bg-center">
+        <div className="w-[400px] h-[500px] max-w-sm p-10 space-y-[25%] bg-blue-500 rounded-lg shadow-lg">
+          <div className="text-center">
+            <h2 className="text-2xl font-bold text-white">LOGIN</h2>
+          </div>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div className="relative mb-10">
+              <FaUser className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white" />
+              <input
+                type="text"
+                name="email"
+                placeholder="EMAIL"
+                onChange={handleInputChange}
+                value={formData.email}
+                className="w-full pl-10 py-2 text-white bg-transparent border-b border-white placeholder-white focus:outline-none focus:border-white"
+              />
+            </div>
+            <div className="relative mb-12">
+              <FaLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white" />
+              <input
+                type="password"
+                name="password"
+                placeholder="PASSWORD"
+                onChange={handleInputChange}
+                value={formData.password}
+                className="w-full pl-10 py-2 text-white bg-transparent border-b border-white placeholder-white focus:outline-none focus:border-white"
+              />
+            </div>
+            <Button onClick={handleLogin} loading={loading} error={error}>
+              Login
+            </Button>
+            <div className="mt-4 text-center">
+              <a href="#" className="text-sm text-white underline">
+                Forgot password?
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Button.tsx
@@ -0,0 +1,40 @@
+import React, { FC, MouseEventHandler, ReactNode } from 'react';
+
+interface ButtonProps {
+    type?: 'button' | 'submit' | 'reset';
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+    id?: string;
+    className?: string;
+    children: ReactNode;
+    loading?: boolean;
+    error?: string;
+}
+
+const Button: FC<ButtonProps> = ({
+    type = 'button',
+    onClick,
+    id,
+    className = '',
+    children,
+    loading = false,
+    error = ''
+}) => {
+    return (
+        <div className="flex flex-col text-center gap-4">
+            <button
+                className={`w-full py-2 font-bold text-blue-600 bg-white rounded focus:outline-none focus:shadow-outline ${className}`}
+                type={type}
+                onClick={onClick}
+                disabled={loading}
+                id={id}
+            >
+                {loading ? "Logging in..." : children}
+            </button>
+            <div className="text-sm">
+                {error && <p style={{ color: 'white' }}>{error}</p>}
+            </div>
+        </div>
+    );
+};
+
+export default Button;
